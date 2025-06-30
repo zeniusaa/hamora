b67ee9364785ab92943672473a64ae2ec9877ed3
zeniusaa <tugasrakazeniusa@gmail.com>
2025-06-30 10:44:42 +0700

    update 4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 android {
     namespace = "com.example.hamora"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.hamora"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -18,7 +18,6 @@ import 'package:provider/provider.dart';
 import 'package:another_flushbar/flushbar.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:random_string/random_string.dart';
 part 'sign_in_page.dart';
 part 'wrapper.dart';
 part 'main_page.dart';
